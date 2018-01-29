06acb456a641d90bf012b7856b2fd1cc86e7332c
Thomas M <marin_t@esme.fr>
2018-01-29 17:03:29 +0200

    Add files via upload

--- final_project_r_2017.R
+++ final_project_r_2017.R
@@ -1,21 +1,46 @@
-#import des différentes lirairies de travail
+#Import des différentes lirairies de travail
 library(data.table)
 library(mclust)
 library(ggplot2)
 library(arules)
 library(rjson)
+library(jsonlite)
+library(dplyr)
+library(recommenderlab)
+library(tidytext)
+library(tidyr)
+library(stringr)
+library(gridExtra)
+
 set.seed(666)
-#le set.seed permet de fixer le générateur aléatoire, qui ne sera plus pris par rapport à l'heure
+#Le set.seed permet de fixer le générateur aléatoire, qui ne sera plus pris par rapport à l'heure
 
 
 
-#import du dataframe puis échantillonnage à 5% du set
-#le dataframe provient d'Amazon, au format JSON et est composé de reviews de jeu-vidéo
-df = fromJSON(file = 'C:/Users/thomas/Downloads/LangageR/ScriptR/Video_Games_5.json')
-df
-r = sample(x = dt, size = length(x*0.05), replace = FALSE)
+#Import du dataframe puis échantillonnage à 5% du set
+#Le dataframe provient d'Amazon, au format JSON et est composé de reviews de jeu-vidéo
+fichier_json = fromJSON(file = 'C:/Users/thomas/Downloads/LangageR/ScriptR/Video_Games_5.json')
+fichier_json
 
 
-#conversion du fichier JSON en dataframe
-jsondf <- as.data.frame(r)
-jsondf
+#echantillon_donnees = sample(x = fichier_json, size = length(x*0.05), replace = FALSE)
+#echantillon_donnees2 <- fichier_json[sample(nrow(fichierjson), 50), ]
+
+
+
+#Conversion du fichier JSON en dataframe
+dataframe_json <- as.data.frame(fichier_json)
+dataframe_json
+
+#On ne garde que reviewername, reviewtext et summary pour l'analyse des sentiments via les textes
+dataframe_final <- dataframe_json[dataframe_json$reviewername, dataframe_json$reviewText, dataframe_json$summary]
+dataframe_final
+
+
+#Début de l'analyse sentimentale
+#La première étape consiste à tokeniser la partie reviewText via la fonction unnest_tokens
+dataframe_final$reviewText <- as.character(dataframe_final$reviewText)
+mots_revue <- dataframe_final %>%
+  select(reviewerName, reviewText) %>%
+  unnest_tokens(word, reviewText) %>%
+  filter(!word %in% stop_words$word, str_detect(word, "^[a-z']+$"))
---
+++ main_tidy_text_analysis.R
@@ -0,0 +1,59 @@
+# Importation des librairies utiles au projet
+library(jsonlite)#installed
+library(dplyr)#installed
+library(recommenderlab)#installed
+library(data.table)#installed
+library(tidytext)#installed
+library(tidyr)#installed
+library(stringr)#installed
+library(gridExtra)#installed
+library(ggplot2)#installed
+
+fichier_dataset = 'C:/Users/thomas/Downloads/LangageR/ScriptR/Video_Games_5.json'
+
+print("Importation du dataset")
+donnees <- stream_in(file(fichier_dataset))
+
+print("Récupération de 10000 échantillons aléatoire")
+echantillon_donnees <- donnees[sample(nrow(donnees), 1), ]
+
+print("Analyse des critiques sentimentales")
+echantillon_donnees$reviewText <- as.character(echantillon_donnees $reviewText)
+mots_revue <- echantillon_donnees %>%
+    select(reviewerID, reviewText, overall) %>%
+    unnest_tokens(word, reviewText) %>%
+    filter(!word %in% stop_words$word, str_detect(word, "^[a-z']+$"))
+
+print("-Nrc")
+nrc <- sentiments %>%
+    filter(sentiment %in% c('positive','negative') & lexicon == 'nrc') %>%
+    mutate(nrc = ifelse(sentiment == 'positive',1,-1)) %>%
+    select(word, nrc)
+
+print("-Bing")
+bing <- sentiments %>%
+    filter(lexicon == 'bing') %>%
+    mutate(bing = ifelse(sentiment == 'positive',1,-1)) %>%
+    select(word, bing)
+
+scores_revues <- mots_revue %>%
+    left_join(nrc, by = 'word') %>%
+    left_join(bing, by = 'word')
+
+sommaire_scores_revues <- scores_revues %>%
+    group_by(reviewerID, overall) %>%
+    summarise(nrc_score = round(mean(nrc, na.rm = T),3),
+    bing_score = round(mean(bing, na.rm = T),3))
+
+print("Affichage des résultats obtenus")
+nrc_graphique <- ggplot(sommaire_scores_revues, aes(x = as.character(overall), y = nrc_score))+
+    geom_boxplot()+
+    labs(x = 'Amazon',
+    y = 'NRC')
+
+bing_graphique <- ggplot(sommaire_scores_revues, aes(x = as.character(overall), y = bing_score))+
+    geom_boxplot()+
+    labs(x = 'Amazon',
+    y = 'Bing')
+
+grid.arrange(nrc_graphique, bing_graphique, nrow = 2)
